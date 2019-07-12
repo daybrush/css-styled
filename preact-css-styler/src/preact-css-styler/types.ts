c12eb969a52e69777ef0686ab3df4df742a06694
Daybrush <daybrush@gmail.com>
2019-07-12 09:46:44 +0900

    fix: fix types

--- preact-css-styler/src/preact-css-styler/types.ts
+++ preact-css-styler/src/preact-css-styler/types.ts
@@ -1,6 +1,7 @@
 import { Component } from "preact";
 import { IObject } from "@daybrush/utils";
 
-export interface StylerInterface extends Component<IObject<any>> {
+export interface StylerInterface extends JSX.ElementClass {
+    props: IObject<any>;
     getElement(): HTMLElement | SVGElement;
 }
