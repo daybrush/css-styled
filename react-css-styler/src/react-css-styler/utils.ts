83d0270d37a1e85d40b2cc2ae8534ec9222c5249
Daybrush <daybrush@gmail.com>
2019-08-03 00:49:44 +0900

    feat: add global pseudo selector

--- react-css-styler/src/react-css-styler/utils.ts
+++ react-css-styler/src/react-css-styler/utils.ts
@@ -9,11 +9,14 @@ export function injectStyle(className: string, css: string) {
 
   style.setAttribute("type", "text/css");
   style.innerHTML = css.replace(/([^}{]*){/mg, (all, selector) => {
-    return splitComma(selector).map(subSelector => 
-      subSelector.indexOf(":host") > - 1
-      ? `${subSelector.replace(/\:host/g, `.${className}`)}`
-      : `.${className} ${subSelector}`
-    ).join(", ") + "{";
+    return splitComma(selector).map(subSelector => {
+      if (subSelector.indexOf(":global") > -1) {
+        return subSelector.replace(/\:global/g, "");
+      } else if (subSelector.indexOf(":host") > -1) {
+        return `${subSelector.replace(/\:host/g, `.${className}`)}`;
+      }
+      return `.${className} ${subSelector}`;
+    }).join(", ") + "{";
   });
 
   (document.head || document.body).appendChild(style);
